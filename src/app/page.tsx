03a2ea5bfa117f86e8188ecc97a4f4ccbeb47783
minjeong.kim <doingnone@gmail.com>
2024-04-25 19:42:24 +0100

    add portfolio router

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
+import Link from 'next/link'
+
 export default function Home() {
   return (
     <div>
       <p>Minjeong Kim / F / South Korea</p>
       <p>Software Engineer living in Ireland</p>
+      <Link href="/portfolios">Portfolios</Link>
     </div>
   );
 }
---
+++ src/app/portfolios/page.tsx
@@ -0,0 +1,38 @@
+type ProjectName = {
+    ko: string
+    en: string
+}
+
+type ReleaseLink = {
+    web?: string
+    iOS?: string
+    Andriod?: string
+}
+
+type Portfolio = {
+    role: string[]
+    using: string[]
+    screenshot: string[]
+    _id: string
+    projectName: ProjectName
+    year: number
+    from: string
+    to: string
+    tag: string[]
+    participation: number
+}
+
+export default async function Portfolios() {
+    const data = await fetch('https://www.minjeong.xyz/data.json')
+    const portfolios: Portfolio[] = await data.json()
+    return(
+        <>
+            <div>Portfolios</div>
+            {portfolios.map((portfolio) => {
+                return(
+                    <div key={portfolio._id}>{portfolio.year} - {portfolio.projectName.ko}</div>
+                )  
+            })}
+        </>
+    )
+}
