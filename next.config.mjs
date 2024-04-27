14822c7208e39739b5315f0bcae86287e90cdc7d
minjeong.kim <doingnone@gmail.com>
2024-04-27 11:27:10 +0100

    footer css

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
     output: 'export',
-};
+    sassOptions: {}
+}
 
-export default nextConfig;
+export default nextConfig
--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,9 +1,17 @@
-import styles from "./footer.module.css"
+import styles from "./footer.module.scss"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLinkedin, faYoutube } from '@fortawesome/free-brands-svg-icons'
+import { faEnvelope } from '@fortawesome/free-regular-svg-icons'
 
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <p>Footer</p>
+      <div className={styles.icons}>
+        <FontAwesomeIcon className={styles.icon} icon={faLinkedin} />
+        <FontAwesomeIcon className={styles.icon} icon={faYoutube} />
+        <FontAwesomeIcon className={styles.icon} icon={faEnvelope} />
+      </div>
+      <p className={styles.copyright}>© 2024 Minjeong Kim</p>
     </footer>
   );
 }
