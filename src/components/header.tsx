a8a4b25b73973a36d36461ad0f5152661fcbd234
minjeong.kim <doingnone@gmail.com>
2024-04-27 13:22:46 +0100

    header css

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,25 @@
+import styles from "./header.module.scss"
+import Image from 'next/image'
+import Link from 'next/link'
+
 export default function Header() {
   return (
-    <header>
-      <p>Header</p>
+    <header className={styles.header}>
+      <div className={styles.logos}>
+        <Image
+          className={styles.logos_profile}
+          src={`/imgs/me.jpeg`}
+          width="60"
+          height="60"
+          alt="me" />
+        <p className={styles.logos_name}>Minjeong Kim</p>
+      </div>
+      <div className={styles.menus}>
+        <Link className={styles.menu}
+          href="/">About Me</Link>
+        <Link className={styles.menu}
+          href="/portfolios">Careers & Portfolios</Link>
+      </div>
     </header>
   );
 }
