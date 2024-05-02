c0f70a3db52ff93ea22412106533bb8da09095ca
minjeong.kim <doingnone@gmail.com>
2024-05-02 05:58:37 +0100

    theming

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-    sassOptions: {}
+    sassOptions: {
+        includePaths: ['./src/styles'],
+        prependData: `@import "variables.scss";`
+    }
 }
 
 export default nextConfig
